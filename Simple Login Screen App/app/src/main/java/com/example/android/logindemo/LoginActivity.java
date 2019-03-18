de251e1899882c933a9206c7adc61d1796c6e71c
sehyun hwang <sehyunny6@gmail.com>
2019-03-18 23:54:28 +0800

    sign up page added

---
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.android.logindemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private Button Signup;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+        Signup=(Button)findViewById(R.id.btnSignup);
+   //     Info.setText("No of attempts remaining: 10");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signupIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+                LoginActivity.this.startActivity(signupIntent);
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if(((userName.equals("sehyun")) && (userPassword.equals("1816")))
+                || ((userName.equals("walid")) && (userPassword.equals("0000")))
+                || ((userName.equals("lucheng")) && (userPassword.equals("1111")))
+                || ((userName.equals("cx")) && (userPassword.equals("2222")))
+                || ((userName.equals("nick")) && (userPassword.equals("3333")))
+          ){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            /*
+            counter--;
+
+            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+            */
+        }
+    }
+
+}
---
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.logindemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+/*
+    EditText idText=(EditText) findViewById(R.id.idText);
+    EditText passwordText=(EditText) findViewById(R.id.passwordText);
+    EditText nameText=(EditText) findViewById(R.id.nameText);
+    EditText emailText=(EditText) findViewById(R.id.emailText);
+    EditText phoneText=(EditText) findViewById(R.id.phoneText);
+
+    Button signupButton=(Button) findViewById(R.id.signupButton);
+  */
+}
