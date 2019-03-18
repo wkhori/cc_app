a9ed3e2ea501388da331e043e907f1c01ed5ac7a
sehyun hwang <sehyunny6@gmail.com>
2019-03-19 01:49:32 +0800

    start main page

--- Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MainActivity.java
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.logindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView1 = (ImageView) findViewById(R.id.sampleLogo) ;
+        imageView1.setImageResource(R.drawable.human) ;
     }
 }
