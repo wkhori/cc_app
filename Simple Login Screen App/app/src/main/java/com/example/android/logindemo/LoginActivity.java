bb82d6405ff43b49948ef7db1a5473e1a0b8a092
Walid Khori <khori005@umn.edu>
2019-03-25 16:45:16 +0800

    changing page layout. Error in activity_login_execution.xml

--- Simple Login Screen App/app/src/main/java/com/example/android/logindemo/LoginActivity.java
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/LoginActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.webkit.WebView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText Password;
     private TextView Info;
     private Button Login;
-    private Button Signup;
+    private Button Register;
     private TextView ErrorMsg;
     private int counter = 5;
 
@@ -24,61 +25,69 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Info = (TextView)findViewById(R.id.tvInfo);
+//        Name = (EditText)findViewById(R.id.etName);
+//        Password = (EditText)findViewById(R.id.etPassword);
+        //Info = (TextView)findViewById(R.id.tvInfo);
         Login = (Button)findViewById(R.id.btnLogin);
-        Signup=(Button)findViewById(R.id.btnSignup);
-        ErrorMsg=(TextView)findViewById(R.id.loginErrorMsg);
+        Register =(Button)findViewById(R.id.btnRegister);
+       // ErrorMsg=(TextView)findViewById(R.id.loginErrorMsg);
 
    //     Info.setText("No of attempts remaining: 10");
-        Name.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ErrorMsg.setVisibility(View.INVISIBLE);
-            }
-        });
+//        Name.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ErrorMsg.setVisibility(View.INVISIBLE);
+//            }
+//        });
 
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                //validate(Name.getText().toString(), Password.getText().toString());
+                Intent LogInIntent = new Intent(LoginActivity.this, ExecuteLogin.class);
+                LoginActivity.this.startActivity(LogInIntent);
             }
         });
-
-        Signup.setOnClickListener(new View.OnClickListener() {
+//
+        Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent signupIntent = new Intent(LoginActivity.this, SignUpActivity.class);
                 LoginActivity.this.startActivity(signupIntent);
             }
         });
+
+//        String html = "<iframe width=\"600\" height=\"400\" src=\"https://data.gov.sg/dataset/community-clubs/resource/da666988-6c48-4f02-9ddf-bd8b11609cfd/view/990fcd0f-857c-4c98-82e8-9b83891d6d8a\" frameBorder=\"0\"> </iframe>";
+//        WebView webview;
+//        webview = (WebView) findViewById(R.id.webview);
+//        webview.getSettings().setJavaScriptEnabled(true);
+//        webview.loadData(html, "text/html", null);
     }
 
-    private void validate(String userName, String userPassword){
-
-        if(((userName.equals("sehyun")) && (userPassword.equals("1816")))
-                || ((userName.equals("walid")) && (userPassword.equals("0000")))
-                || ((userName.equals("lucheng")) && (userPassword.equals("1111")))
-                || ((userName.equals("cx")) && (userPassword.equals("2222")))
-                || ((userName.equals("nick")) && (userPassword.equals("3333")))
-          ){
-            ErrorMsg.setVisibility(View.INVISIBLE);
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-        }else{
-            ErrorMsg.setVisibility(View.VISIBLE);
-            /*
-            counter--;
-
-            Info.setText("No of attempts remaining: " + String.valueOf(counter));
-
-            if(counter == 0){
-                Login.setEnabled(false);
-            }
-            */
-        }
-    }
+//    private void validate(String userName, String userPassword){
+//
+//        if(((userName.equals("sehyun")) && (userPassword.equals("1816")))
+//                || ((userName.equals("walid")) && (userPassword.equals("0000")))
+//                || ((userName.equals("lucheng")) && (userPassword.equals("1111")))
+//                || ((userName.equals("cx")) && (userPassword.equals("2222")))
+//                || ((userName.equals("nick")) && (userPassword.equals("3333")))
+//          ){
+//            ErrorMsg.setVisibility(View.INVISIBLE);
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+//        }else{
+//            ErrorMsg.setVisibility(View.VISIBLE);
+//            /*
+//            counter--;
+//
+//            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+//
+//            if(counter == 0){
+//                Login.setEnabled(false);
+//            }
+//            */
+//        }
+//    }
 
 }
