d52778f3c7fb1c2d50aaf241360af823e16f6836
sehyun hwang <sehyunny6@gmail.com>
2019-03-27 23:42:03 +0800

    added bottom navigation

--- Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MainActivity.java
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.logindemo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +15,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView1 = (ImageView) findViewById(R.id.sampleLogo) ;
-        imageView1.setImageResource(R.drawable.human) ;
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment=null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_courses:
+                            selectedFragment = new CoursesFragment();
+                            break;
+                        case R.id.nav_map:
+                            selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_me:
+                            selectedFragment = new MeFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
