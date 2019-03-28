895d92f5b884552427668b854a31ac13a76c9dec
Walid Khori <khori005@umn.edu>
2019-03-28 15:58:20 +0800

    map page complete

--- Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MapFragment.java
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MapFragment.java
@@ -20,7 +20,7 @@ public class MapFragment extends Fragment {
       //  return inflater.inflate(R.layout.fragment_map,container,false);
 
         View v = inflater.inflate(R.layout.fragment_map,container,false);
-        String html = "<iframe width=\"600\" height=\"400\" src=\"https://data.gov.sg/dataset/community-clubs/resource/da666988-6c48-4f02-9ddf-bd8b11609cfd/view/990fcd0f-857c-4c98-82e8-9b83891d6d8a\" frameBorder=\"0\"> </iframe>";
+        String html = "<iframe width=\"400\" height=\"600\" src=\"https://data.gov.sg/dataset/community-clubs/resource/da666988-6c48-4f02-9ddf-bd8b11609cfd/view/990fcd0f-857c-4c98-82e8-9b83891d6d8a\" frameBorder=\"0\"> </iframe>";
         WebView webview = (WebView) v.findViewById(R.id.webview);
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
