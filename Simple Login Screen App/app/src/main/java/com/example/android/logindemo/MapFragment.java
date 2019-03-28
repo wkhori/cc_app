b2186ba19095b5936d8ca4633729bb804a9eb063
Walid Khori <khori005@umn.edu>
2019-03-28 15:49:33 +0800

    map functionality added to map page

--- Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MapFragment.java
+++ Simple Login Screen App/app/src/main/java/com/example/android/logindemo/MapFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 public class MapFragment extends Fragment {
     @Nullable
@@ -16,7 +17,15 @@ public class MapFragment extends Fragment {
 //        WebView webview;
 //        webview = (WebView) getView().findViewById(R.id.webview);
 //        webview.loadData(html, "text/html", null);
-        return inflater.inflate(R.layout.fragment_map,container,false);
+      //  return inflater.inflate(R.layout.fragment_map,container,false);
+
+        View v = inflater.inflate(R.layout.fragment_map,container,false);
+        String html = "<iframe width=\"600\" height=\"400\" src=\"https://data.gov.sg/dataset/community-clubs/resource/da666988-6c48-4f02-9ddf-bd8b11609cfd/view/990fcd0f-857c-4c98-82e8-9b83891d6d8a\" frameBorder=\"0\"> </iframe>";
+        WebView webview = (WebView) v.findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadData(html, "text/html", null);
+        return v;
     }
 
 
